13b0b9ca0168b7e4bdf0f33419a03aaafaa39b1b
Creadorus <69775484+Creadorus@users.noreply.github.com>
2020-08-18 14:20:05 +0900

    po

---
+++ AngularSample/src/app/order-supplier-select/order-supplier-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-supplier-select',
+  templateUrl: './order-supplier-select.component.html',
+  styleUrls: ['./order-supplier-select.component.css']
+})
+export class OrderSupplierSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
