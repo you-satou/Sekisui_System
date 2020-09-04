46c0902dc21410ebb4f978a41727484277db02af
yuriekanatsu <69831292+yuriekanatsu@users.noreply.github.com>
2020-09-04 17:35:49 +0900

    9/4 画面→モーダル

--- AngularSample/src/app/ODIS0050/component/supplier-pattern.component.ts
+++ AngularSample/src/app/ODIS0050/component/supplier-pattern.component.ts
@@ -1,40 +1,34 @@
 import { Component, OnInit, Output, EventEmitter, ChangeDetectorRef, ElementRef } from '@angular/core';
-import { WkAllItemTypesService } from '../../wk-all-item-types.service';
-import { WkAllItemType } from '../../WkAllItemType';
+import { WkAllItemTypesService } from './../../wk-all-item-types.service'
+import { WkAllItemType } from './../../WkAllItemType';
 import { Router } from '@angular/router';
 import { SupplierPatternService } from '../services/supplier-pattern.service';
 import { PatternType } from '../entities/odis0050.entity';
 import { SupplierType } from '../entities/odis0050.entity';
-import { pTestdata, sTestdata } from '../test-data';
+import { pTestdata } from '../test-data'
+import { sTestdata } from '../test-data'
 import { CommonComponent } from '../../common/common.component'
-import { AppComponent } from '../../app.component'
-import { Const } from '../../common/const'
 
 @Component({
     selector: 'supplier-pattern',
     templateUrl: './supplier-pattern.component.html',
-    styleUrls: ['../../common/common.component.css',
+    styleUrls: ['./../../common/common.component.css',
                 './supplier-pattern.component.css']
   })
 
 export class SupplierPatternComponent implements OnInit {
 
-  title = "発注先パターン選択";
+  title = "発注明細入力_発注先パターン選択";
 
-   // Todo
+  // Todo
   pDatas: PatternType[] = pTestdata;
   sDatas: SupplierType[] = sTestdata;
   constructor(
     private modalService: SupplierPatternService,
-    private appComponent: AppComponent,
+    private commonComponent: CommonComponent,
+
   ){}
-
-
-  ngOnInit() {
-
-    this.appComponent.setHeader(Const.ScreenName.S0003,Const.LinKSetting.L0003);
-
-  }
+  ngOnInit() {}
   ngOnDestroy() {
     // モーダルダイアログが閉じたタイミングで出力される
     console.log('destroyed');
@@ -57,15 +51,8 @@ export class SupplierPatternComponent implements OnInit {
   public onSelHighLight($event, selectedItem){
     // テーブル 背景色 クリア
     var wTbody = $event.target.parentElement.parentElement;
-    for(var i=0; i<wTbody.rows.length; i++){
-      // 行 取得
-      var wTr = wTbody.rows[i];
-      for(var j=0; j<wTr.cells.length; j++){
-        // セル クリア
-        var wTd = wTr.cells[j];
-        wTd.style.backgroundColor = '';
-      }
-    }
+
+    this.commonComponent.CommonOnSelHight($event);
 
     // 要素取得
     var wTr = $event.target.parentElement;
