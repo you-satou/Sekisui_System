6fd5932a17e4d7f0b37a6b3c2967f2a9435bca12
quanle19thh <quan_le@fsz.co.jp>
2020-09-09 19:08:49 +0900

    Order-Detail-Input-Entities 分けた

---
+++ AngularSample/src/app/ODIS0020/entities/odis0020-MainOrderEdaBan.entity.ts
@@ -0,0 +1,33 @@
+/**
+ * 本体受注枝番テーブルの定義
+ */
+export interface ODIS0020MainOrderEdaBan{
+
+    /**  契約種別 */
+    contractModel: string;
+
+    /**  受注形態 */
+    orderModel: string;
+
+    /**  枝番 1*/
+    branchNum1: string;
+
+    /**  枝番 2 */
+    branchNum2: string;
+
+    /**  契約年月日 */
+    date: string;
+
+    /**  金額 */
+    contractAmount: string;
+
+    /**   発注状況 */
+    completeStatus: string;
+
+    /**   売上状況 */
+    salesStatus: string;
+
+    /**   売上年月日 */
+    salesUpdateDate: string;
+
+}
---
+++ AngularSample/src/app/ODIS0020/entities/odis0020-OrderInfomation.entity.ts
@@ -0,0 +1,90 @@
+/**
+ * 発注明細 邸情報の定義
+ */
+export interface ODIS0020OrderDetailInputInformation {
+
+    /** 発注番号 */
+    contractNum: string;
+
+    /** 工事負担事業所コード */
+    officeCode: string;
+
+    /** 事業所名 */
+    officeName: string;
+
+    /** 営業担当コード */
+    salesStaffCode: string;
+
+    /** 営業担当名 */
+    salesStaffName: string;
+
+    /** 建築担当番号 */
+    architectCode: string;
+
+    /** 建築担当者 */
+    architectName: string;
+
+    /** 工事物件名カナ */
+    constructionKata: string;
+
+    /** 工事物件名漢字 */
+    constructionHira: string;
+
+    /** 契約書番号 */
+    contractorCode: string;
+
+    /** 契約者名漢字 */
+    contractorName: string;
+
+    /*-------------工事名のテーブルの定義---------------------*/
+
+    /** NULL(固定) */
+    planContractDate: string;
+
+    /** 当初契約年月日 */
+    perforContractDate: string;
+
+    /** 工場オーダー予定日 */
+    planDODate: string;
+
+    /** 工場オーダー実績日 */
+    perforDODate: string;
+
+    /** 基礎着工予定日 */
+    planStartDate: string;
+
+    /** 基礎着工実績日 */
+    perforStartDate: string;
+
+    /** 外装材入荷予定日 */
+    planArrivalDate: string;
+
+    /** 外装材入荷実績日 */
+    perforArrivalDate: string;
+
+    /** 木工完了予定日 */
+    planWoodWorkDate: string;
+
+    /** 木工完了実績日 */
+    perforWoodWorkDate: string;
+
+    /** 総合検査予定日 */
+    planInspectionDate: string;
+
+    /** 総合検査実績日 */
+    perforInspectionDate: string;
+
+    /** 竣工検査予定日 */
+    planCompletionInspection: string;
+
+    /** 竣工検査実績日 */
+    perforCompletionInspection: string;
+
+    /** 建物引渡し予定日 */
+    planHandingOver: string;
+
+    /** 建物引渡し実績日 */
+    perforHandingOver: string;
+
+
+}
