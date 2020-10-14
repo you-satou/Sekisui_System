d4c8c475fbdf6a75eb11762f4e1b8872c4f0c493
yuriekanatsu <69831292+yuriekanatsu@users.noreply.github.com>
2020-10-14 13:52:51 +0900

    TabIndex

--- AngularSample/src/app/ODIS0010/component/paginator/order-paginator.directive.ts
+++ AngularSample/src/app/ODIS0010/component/paginator/order-paginator.directive.ts
@@ -65,6 +65,7 @@ export class OrderApprovalPaginator extends MatPaginatorIntl {
         this.rend.setAttribute(inputIndex, 'type', 'tel');
         this.rend.setAttribute(inputIndex, 'disabled', 'true');
         this.rend.setAttribute(inputIndex, 'id', 'pageIndex');
+        this.rend.setAttribute(inputIndex, 'tabIndex', '13');
         // this.rend.setStyle(inputIndex,'display','disabled');
         //ＥＮＴＥＲキーを押下する時のエベントハンドラーを設定する
         this.rend.listen(inputIndex, 'keypress', ($event) => {
@@ -107,10 +108,14 @@ export class OrderApprovalPaginator extends MatPaginatorIntl {
   //各ボタンにBOOTSTRAPクラスに追加する
   setNewClass(){
     let buttonNodes = document.querySelectorAll('div.mat-paginator-range-actions > button');
+    let btnTabIndex = 11;
     buttonNodes.forEach(element => {
 
       element.classList.add('btn');
       element.classList.add('btn-link');
+      if(btnTabIndex === 13) btnTabIndex++;
+      element.setAttribute('tabIndex', (btnTabIndex).toString())
+      btnTabIndex++;
     });
   }
 
