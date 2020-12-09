880d27fc4b4147f3c327b3b77d1dd4857679d51a
yasuishota <69831090+yasuishota@users.noreply.github.com>
2020-12-09 18:21:47 +0900

    発注、支払、受入　データ取得

--- AngularSample/src/app/common/const.ts
+++ AngularSample/src/app/common/const.ts
@@ -52,6 +52,7 @@ export namespace Const{
         export const S0002_UPDATE = '/ODIS0020/update';
         export const S0002_GetOrderDetailFile = '/ODIS0020/download';
         export const S0002_UpdateAndDownload = '/ODIS0020/updateAndDownload';
+        export const S0002_GetSuchOAP = '/ODIS0020/getSuchOAP';
         export const S0003_Init = '/ODIS0030/init';
         export const S0004_Init = '/ODIS0040/init';
         export const S0005_Init = '/ODIS0050/init';
